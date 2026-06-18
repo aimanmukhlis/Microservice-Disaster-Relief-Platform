7b980a6710445b004f0c75592979a98a1275a526
aimanmukhlis <aimanmukhlis7@gmail.com>
2026-06-18 12:16:29 +0800

    Change

--- Inventory/Inventory_Backend/index.js
+++ Inventory/Inventory_Backend/index.js
@@ -171,7 +171,7 @@ async function startServer() {
         });
 
         const PORT = process.env.PORT || 7020;
-        app.listen(PORT, () => {
+        app.listen(PORT,'0.0.0.0', () => {
             console.log(`🚀 Inventory Microservice backend running on port ${PORT}`);
         });
 
